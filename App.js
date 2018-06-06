9e93f4272ccc74cd28f47a7a8be9953b0342f458
Carlos Aguilera <a.carlosing@gmail.com>
2018-06-05 21:19:29 -0500

    Working on the app: Adding a textinput.

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+    state = {
+        placeName: ''
+    };
+
+    placeNameChangedHandler = val => {
+        this.setState({
+            placeName: val
+        });
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <TextInput
+                    style={{ width: 300, borderColor: 'black', borderWidth: 1 }}
+                    value={this.state.placeName}
+                    onChangeText={this.placeNameChangedHandler}
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
 });
