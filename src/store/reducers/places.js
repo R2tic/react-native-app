06108ba6cee90cbf75239df9aecc1d2fd8370dd0
Carlos Aguilera <a.carlosing@gmail.com>
2018-06-08 16:24:45 -0500

    Setting up the reducer.

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,3 +1,6 @@
+import * as actions from '../actions/actionTypes';
+import { stat } from 'fs';
+
 const initialStae = {
     places: [],
     selectedPlace: null
@@ -5,6 +8,38 @@ const initialStae = {
 
 const reducer = (state = initialStae, action) => {
     switch (action.type) {
+        case actions.ADD_PLACE:
+            return {
+                ...state,
+                places: state.places.concat({
+                    key: String(Math.random()),
+                    name: action.placeName,
+                    image: {
+                        uri:
+                            'https://www.publicdomainpictures.net/pictures/150000/velka/tropical-beach-1454007190ZAK.jpg'
+                    }
+                })
+            };
+        case actions.DELETE_PLACE:
+            return {
+                ...state,
+                places: state.places.filter(place => {
+                    return place.key !== state.selectedPlace.key;
+                }),
+                selectedPlace: null
+            };
+        case actions.SELECT_PLACE:
+            return {
+                ...state,
+                selectedPlace: state.places.find(place => {
+                    return place.key === action.placeKey;
+                })
+            };
+        case actions.DESELECT_PLACE:
+            return {
+                ...state,
+                selectedPlace: null
+            };
         default:
             return state;
     }
