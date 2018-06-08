fb75f4a295870b901530ac9a1c5a0bb4b75b737b
Carlos Aguilera <a.carlosing@gmail.com>
2018-06-08 16:08:32 -0500

    Installing redux and creating a basic setup.

---
+++ src/store/reducers/places.js
@@ -0,0 +1,13 @@
+const initialStae = {
+    places: [],
+    selectedPlace: null
+};
+
+const reducer = (state = initialStae, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default reducer;
