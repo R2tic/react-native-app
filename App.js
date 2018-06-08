bfbbbbd293c4211e6222c264dbebd7052ca4afa3
Carlos Aguilera <a.carlosing@gmail.com>
2018-06-08 12:47:45 -0500

    Adding a modal.

--- App.js
+++ App.js
@@ -3,17 +3,19 @@ import { StyleSheet, View } from 'react-native';
 
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 import PlaceList from './src/components/PlaceList/PlaceList';
+import PlaceDetails from './src/components/PlaceDetail/PlaceDetail';
 
 export default class App extends React.Component {
     state = {
-        places: []
+        places: [],
+        selectedPlace: null
     };
 
     placeAddedHandler = placeName => {
         this.setState(prevState => {
             return {
                 places: prevState.places.concat({
-                    key: Math.random(),
+                    key: String(Math.random()),
                     name: placeName,
                     image: {
                         uri:
@@ -24,23 +26,43 @@ export default class App extends React.Component {
         });
     };
 
-    placeDeletedHandler = key => {
+    placeSelectedHandler = key => {
         this.setState(prevState => {
             return {
-                places: prevState.places.filter(place => {
-                    return place.key !== key;
+                selectedPlace: prevState.places.find(place => {
+                    return place.key === key;
                 })
             };
         });
     };
 
+    placeDeletedHandler = () => {
+        this.setState(prevState => {
+            return {
+                places: prevState.places.filter(place => {
+                    return place.key !== prevState.selectedPlace.key;
+                }),
+                selectedPlace: null
+            };
+        });
+    };
+
+    modalClosedHandler = () => {
+        this.setState({ selectedPlace: null });
+    };
+
     render() {
         return (
             <View style={styles.container}>
+                <PlaceDetails
+                    selectedPlace={this.state.selectedPlace}
+                    onItemDeleted={this.placeDeletedHandler}
+                    onModalClosed={this.modalClosedHandler}
+                />
                 <PlaceInput onPlaceAdded={this.placeAddedHandler} />
                 <PlaceList
                     places={this.state.places}
-                    onItemDeleted={this.placeDeletedHandler}
+                    onItemSelected={this.placeSelectedHandler}
                 />
             </View>
         );
