9bcd99a3a4a97f03519900137fd4ab5815c89774
Carlos Aguilera <a.carlosing@gmail.com>
2018-06-22 09:07:35 -0500

    Adding a Side Drawer.

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
 import PlaceDetailScreen from './src/screens/PlaceDetail/PlaceDetail';
+import SideDrawer from './src/screens/SideDrawer/SideDrawer';
 import configureStore from './src/store/configureStore';
 
 const store = configureStore();
@@ -31,6 +32,12 @@ Navigation.registerComponent(
 Navigation.registerComponent(
     'awesome-places.PlaceDetailScreen',
     () => PlaceDetailScreen,
+    store,
+    Provider
+);
+Navigation.registerComponent(
+    'awesome-places.SideDrawer',
+    () => SideDrawer,
     store,
     Provider
 );
